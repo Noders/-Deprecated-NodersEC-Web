619252cef7cb85e55f26889716e77322eb270b68
Ramon Chancay <ramon.chancay@gmail.com>
2015-05-12 20:34:02 -0500

    cambios en el frontend

--- server.js
+++ server.js
@@ -3,14 +3,12 @@ var app   = require('./config/express')(),
 	  index = require('./app/routes/index'),
 		error = require('./app/routes/errors'),
 		twitt = require('./app/routes/twitter'),
-		auth  = require('./app/routes/auth'),
 		mongoose  =require('mongoose');
 
 		mongoose.connect(process.env.MONGOURI);
 app.use(index);
 
 app.use(twitt);
-app.use(auth);
 app.use(error);
 
 var server = app.listen(8080);
