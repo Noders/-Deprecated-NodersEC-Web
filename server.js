9662aa1bc361a127e30dadc476092823c3f97e9b
Ramon Chancay <ramon.chancay@gmail.com>
2015-05-15 00:00:57 -0500

    agregando area 51

--- server.js
+++ server.js
@@ -4,10 +4,9 @@ var app   = require('./config/express')(),
 		error = require('./app/routes/errors'),
 		twitt = require('./app/routes/twitter'),
 		mongoose  =require('mongoose');
-
 		mongoose.connect(process.env.MONGOURI);
-app.use(index);
 
+app.use(index);
 app.use(twitt);
 app.use(error);
 
