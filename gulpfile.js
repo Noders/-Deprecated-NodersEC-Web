ab948d2e26dcb0cdb031c1b016f3688acd3f5d18
Ramon Chancay <ramon.chancay@gmail.com>
2015-05-19 17:20:44 -0500

    modify gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,22 @@
-var gulp       = require('gulp'),
+var gulp     = require('gulp'),
 	gulpif     = require('gulp-if'),
 	minifyCss  = require('gulp-minify-css'),
 	useref     = require('gulp-useref'),
 	uglify     = require('gulp-uglify'),
-	uncss      = require('gulp-uncss');
+	uncss      = require('gulp-uncss'),
+	inject     = require('gulp-inject');
 
+
+gulp.task('inject',function(){
+	var target = gulp.src('./app/views/index.html');
+	var sources = gulp.src(['./public/css/**/*.js', './public/css/lib/**/*.css'], {read: false});
+	return target.pipe(inject(sources))
+    .pipe(gulp.dest('./app/views'));
+})
 
 gulp.task('compress',function() {
 	var assets = useref.assets(),
 	opts = { comments:false };
-
 	gulp.src('./app/views/index.html')
 		.pipe(assets)
 		.pipe(gulpif('*.css',minifyCss({keepSpecialComments : 0})))
@@ -18,3 +25,6 @@ gulp.task('compress',function() {
 		.pipe(useref())
 		.pipe(gulp.dest('./build/'));
 });
+
+
+gulp.task('build',['compress']);
