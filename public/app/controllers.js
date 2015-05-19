9ac24a1b89a8ff730d980339bc34f874d31cade1
Ramon Chancay <ramon.chancay@gmail.com>
2015-05-19 17:08:38 -0500

    add bower

---
+++ public/app/controllers.js
@@ -0,0 +1,41 @@
+angular.module('app')
+
+.controller('TwittsController', ['$scope','$http', '$q', function($scope,$http,$q) {
+
+    function refreshCarrousel(){
+      setTimeout(function(){
+        $(".testemonials").owlCarousel({
+          autoPlay: 8000,
+          autoHeight : true,
+          singleItem: true,
+          navigation: false,
+          itemsDesktop: [1000, 1],
+              itemsDesktopSmall: [900, 1],
+              itemsTablet: [600, 1],
+              itemsMobile: false
+        });
+        $("#reviews").removeClass('hide');
+      },1000);
+    }
+
+    function getTwitts(){
+      var d = $q.defer();
+
+      $http.get('/twitts').
+       success(function(data, status, headers, config) {
+         d.resolve(data);
+       }).
+       error(function(data, status, headers, config) {
+         d.reject(data);
+       });
+
+       return d.promise;
+    }
+
+    getTwitts()
+    .then(function(data){
+        $scope.twitts = data;
+        refreshCarrousel();
+    });
+
+}])
